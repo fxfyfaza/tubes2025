2243d8d3b5825694914bb85169e8f6132c48b85d
FXFYFZ <fazafajar69@gmail.com>
2025-05-22 14:31:01 +0700

    data update

--- teskirimdata.py
+++ teskirimdata.py
@@ -2,15 +2,12 @@ import requests
 import time
 import random
 
-# GANTI dengan Write API Key dari channel kamu
 WRITE_API_KEY = '7DFPC06L1AAX11M9'
 
-# URL endpoint ThingSpeak
 url = 'https://api.thingspeak.com/update'
 
 def kirim_data_dummy():
     while True:
-        # Data dummy: suhu 20-40 °C dan kecepatan kipas 1000-2000 RPM
         suhu = round(random.uniform(20, 40), 2)
         kipas = random.randint(1000, 2000)
 
@@ -25,9 +22,9 @@ def kirim_data_dummy():
         if response.status_code == 200 and response.text != '0':
             print(f'✅ Data terkirim: Suhu={suhu} °C, Kipas={kipas} RPM (entry #{response.text})')
         else:
-            print('❌ Gagal kirim data:', response.text)
+            print('Gagal kirim data:', response.text)
 
-        time.sleep(15)  # Tunggu 15 detik (ThingSpeak rate limit)
+        time.sleep(15)  
         
 if __name__ == "__main__":
     kirim_data_dummy()
